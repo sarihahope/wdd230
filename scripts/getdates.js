79eb87acad181de8407db7f38e9e153909a100ae
sarihahope <sarihaslink@yahoo.com>
2023-09-28 20:52:07 -0700

    Completed menu

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,3 +2,11 @@ let text = document.lastModified;
 document.getElementById("lastmodified").innerHTML = text;
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
