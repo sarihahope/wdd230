942dbd18264e3222258c33d60d26bf058ab43c85
sarihahope <sarihaslink@yahoo.com>
2023-10-07 11:13:44 -0700

    Chamber project

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,11 @@
+let text = document.lastModified;
+document.getElementById("lastmodified").innerHTML = text;
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
