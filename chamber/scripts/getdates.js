d03c3ef6f37917b6edfc35772a4a50c1b73ff172
sarihahope <sarihaslink@yahoo.com>
2023-10-12 21:31:54 -0700

    menu and dark

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -9,3 +9,21 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+
+
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
