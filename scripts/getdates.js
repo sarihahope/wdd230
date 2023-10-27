a60211f325a603548f2b29fc8c8f12f2b875212d
sarihahope <sarihaslink@yahoo.com>
2023-10-26 18:18:28 -0700

    Added visits

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -10,3 +10,29 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+
+
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+
+
+
+var temp= 91;
+var wSpeed= 15;
+var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+
+var windChill= Math.round(windChill);
+document.getElementById("windChill").innerHTML= windChill;
