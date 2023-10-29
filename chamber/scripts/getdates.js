8b9b6fa0382afe8f9f8fb9f06e6d5ec59eea0eba
sarihahope <sarihaslink@yahoo.com>
2023-10-28 18:01:22 -0700

    Updated items

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -27,3 +27,21 @@ modeButton.addEventListener("click", () => {
 		modeButton.textContent = "🕶️";
 	}
 });
+
+
+
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
+}
+
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+
