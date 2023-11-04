deefc0577adf9a693ee0fc1e8e158b49cbd0aaf1
sarihahope <sarihaslink@yahoo.com>
2023-11-04 14:10:09 -0700

    Completed form

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -45,3 +45,11 @@ numVisits++;
 
 localStorage.setItem("numVisits-ls", numVisits);
 
+var d = new Date();
+
+document.getElementById("date").value = d.toDateString();
+
+var hours = d.getHours();
+var mins = d.getMinutes();
+var seconds = d.getSeconds();
+document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
