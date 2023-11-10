5f03ab514d9a31c6026116fcc5c890b7c9d9ef8e
sarihahope <sarihaslink@yahoo.com>
2023-11-09 19:03:08 -0800

    json

--- scripts/links.js
+++ scripts/links.js
@@ -4,17 +4,24 @@ const linksURL = "https://sarihahope.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
-    // displayLinks(data);
+    //console.log(data);
+    displayLinks(data);
   }
   
   getLinks();
 
 
-//   const displayLinks = (lessons) => {
-//     lessons.forEach((lesson) => {
+const displayLinks = (weeks) => {
+    weeks.forEach((link) => {
 
-  
+        let card = document.createElement('section');
+        let week = document.createElement('url')
+        let link = document.createElement('title')
 
-//     }); 
-//   }
+        card.appendChild(week); //fill in the blank
+        card.appendChild(url);
+    
+        card.appendChild(card);
+
+    }); 
+  }
