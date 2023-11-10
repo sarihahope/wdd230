91279dbef754514ee87162a51778b9533e80b334
sarihahope <sarihaslink@yahoo.com>
2023-11-09 23:16:52 -0800

    Links

--- scripts/links.js
+++ scripts/links.js
@@ -1,27 +1,31 @@
 const baseURL = "https://sarihahope.github.io/wdd230/";
 const linksURL = "https://sarihahope.github.io/wdd230/data/links.json";
+const links = document.getElementById("links");
+
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
     //console.log(data);
-    displayLinks(data);
+    displayLinks(data.lessons);
   }
   
-  getLinks();
 
 
-const displayLinks = (weeks) => {
-    weeks.forEach((link) => {
 
-        let card = document.createElement('section');
-        let week = document.createElement('url')
-        let link = document.createElement('title')
+  const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+        console.log(week);
+        let weekLinks = document.createElement("li");
+        let sentence = `Week ${week.lesson}: `;
+        week.links.forEach((link, index) => {
 
-        card.appendChild(week); //fill in the blank
-        card.appendChild(url);
-    
-        card.appendChild(card);
-
-    }); 
-  }
+            if (index + 1 == week.links.length) {
+                sentence += `<a href=${link.url}>${link.title}</a>`;
+            }
+        });
+        weekLinks.innerHTML = sentence
+        links.appendChild(weekLinks);
+    });
+}
+getLinks();
