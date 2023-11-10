f37a01b937a584c978b559636e0927d01794e608
sarihahope <sarihaslink@yahoo.com>
2023-11-09 18:36:44 -0800

    fixed link

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,20 @@
 const baseURL = "https://sarihahope.github.io/wdd230/";
 const linksURL = "https://sarihahope.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+    // displayLinks(data);
+  }
+  
+  getLinks();
+
+
+//   const displayLinks = (lessons) => {
+//     lessons.forEach((lesson) => {
+
+  
+
+//     }); 
+//   }
